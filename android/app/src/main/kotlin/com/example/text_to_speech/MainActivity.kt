4659f04c4ece2971c5a106801353f7bcf9e580bb
Bemyself777 <felixt1117@gmail.com>
2025-03-07 23:55:50 +0800

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/text_to_speech/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/text_to_speech/MainActivity.kt
@@ -1,6 +1,6 @@
-package com.example.text_to_speech
+package com.yourpackage
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
 }
